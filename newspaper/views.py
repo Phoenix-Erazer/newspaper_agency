f66b5a2d19ebefff00d55e071d05518b6e60187b
Andii Rashevskyi <phoenixerazer@gmail.com>
2023-07-01 21:46:23 +0300

    Added base.html, index.html, sidebar, newspaper/urls.py, styles.ccs

--- newspaper/views.py
+++ newspaper/views.py
@@ -1,1 +1,17 @@
 from django.shortcuts import render
+
+from newspaper.models import Topic, Redactor, Newspaper
+
+
+def index(request):
+    num_topics = Topic.objects.count()
+    num_redactors = Redactor.objects.count()
+    num_newspapers = Newspaper.objects.count()
+
+    context = {
+        "num_topics": num_topics,
+        "num_redactors": num_redactors,
+        "num_newspapers": num_newspapers,
+    }
+
+    return render(request, "newspaper/index.html", context=context)
